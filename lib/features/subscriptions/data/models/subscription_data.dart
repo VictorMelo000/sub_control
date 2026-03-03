4737b229859d97cd985417774d673ee632a373c1
Victor Oliveira Melo <vmelo8044@gmail.com>
2026-03-03 12:21:08 -0400

    Initial structure

---
+++ lib/features/subscriptions/data/models/subscription_data.dart
@@ -0,0 +1,34 @@
+import '../../domain/entities/subscription.dart';
+
+List<Subscription> subscriptionData = [
+  Subscription(
+    id: 1,
+    name: 'Netlix',
+    price: 64,
+    dueDate: DateTime(2025, 10, 18),
+  ),
+  Subscription(
+    id: 2,
+    name: 'HBO MAX',
+    price: 70,
+    dueDate: DateTime(2025, 09, 30),
+  ),
+  Subscription(
+    id: 3,
+    name: 'Disney+',
+    price: 50,
+    dueDate: DateTime(2025, 05, 25),
+  ),
+  Subscription(
+    id: 4,
+    name: 'Amazon Prime',
+    price: 130,
+    dueDate: DateTime(2025, 02, 02),
+  ),
+  Subscription(
+    id: 5,
+    name: 'Apple TV',
+    price: 64,
+    dueDate: DateTime(2025, 09, 12),
+  ),
+];
